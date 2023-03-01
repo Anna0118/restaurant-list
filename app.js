4d231c7ca374f36aa6b100bbcce623779bb35a46
Anna0118 <annachu0988@gmail.com>
2023-03-02 01:03:26 +0800

    feat: add restaurant seeder

--- app.js
+++ app.js
@@ -1,56 +1,75 @@
 // require packages used in the project
 const express = require("express");
+const exphbs = require("express-handlebars"); // 告訴express, 要使用handlebars, 來解析html
+const mongoose = require("mongoose"); // 載入mongoose
+const bodyParser = require("body-parser"); // 引用 body-parser
+const Restaurant = require("./models/Restaurant");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); // 設定連線到 mongoDB
+
+const db = mongoose.connection; // 用on註冊監聽器，監聽error事件有沒有發生,只要觸error就印出error訊息
+db.on("error", () => {
+  console.log("mongodb error");
+});
+// 針對連線成功的open其情況,連線成功只會發生一次，所以特地用once，一但成功後，在執行callback以後就會解除監聽器
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+
+// //透過路徑檔, 將需要用到的資料require近來
+// const restList = require("./restaurant.json");
 const app = express();
 const port = 3000;
-
-// 告訴express, 要使用handlebars, 來解析html
-const exphbs = require("express-handlebars");
-
-//透過路徑檔, 將需要用到的資料require近來
-const restList = require("./restaurant.json");
 
 // setting template engine
 // app.enging(參數1:要只用的樣版引擎, 參數2: 使用這個引擎的相關設定)
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// 告訴 Express 說要設定的 view engine 是 handlebars
-app.set("view engine", "handlebars");
-
+app.set("view engine", "handlebars"); // 告訴 Express 說要設定的 view engine 是 handlebars
 // setting static files
-// 告訴 Express 靜態檔案是放在名為 public 的資料夾中
-app.use(express.static("public"));
+app.use(express.static("public")); // 告訴 Express 靜態檔案是放在名為 public 的資料夾中
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// routes setting
-app.get("/", (req, res) => {
-  // past the movie data into 'index' partial template
-  res.render("index", { restaurants: restList.results });
-});
 
-// route setting for show another page
-app.get("/restaurants/:restaurant_id", (req, res) => {
-  const restaurant = restList.results.find(
-    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
-  );
-  res.render("show", { restaurant: restaurant });
-});
+// // routes setting
+// app.get("/", (req, res) => {
+//   // past the movie data into 'index' partial template
+//   res.render("index", { restaurants: restList.results });
+// });
 
-// query string
-// 透過req.query 可得網頁?後的東西
-// 設定對應的路由器
-app.get("/search", (req, res) => {
-  // 如果沒有輸入關鍵字, 返回原始畫面
-  if (!req.query.keyword) {
-    return res.redirect("/");
-  }
-  //   const keywords = req.query.keywords;
-  const keyword = req.query.keyword.trim().toLowerCase();
+// // route setting for show another page
+// app.get("/restaurants/:restaurant_id", (req, res) => {
+//   const restaurant = restList.results.find(
+//     (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+//   );
+//   res.render("show", { restaurant: restaurant });
+// });
 
-  const restaurants = restList.results.filter(
-    (data) =>
-      data.name.toLowerCase().includes(keyword) ||
-      data.category.includes(keyword)
-  );
-  res.render("index", { restaurants: restaurants, keyword: keyword });
-});
+// // query string
+// // 透過req.query 可得網頁?後的東西
+// // 設定對應的路由器
+// app.get("/search", (req, res) => {
+//   // 如果沒有輸入關鍵字, 返回原始畫面
+//   if (!req.query.keyword) {
+//     return res.redirect("/");
+//   }
+//   //   const keywords = req.query.keywords;
+//   const keyword = req.query.keyword.trim().toLowerCase();
+
+//   const restaurants = restList.results.filter(
+//     (data) =>
+//       data.name.toLowerCase().includes(keyword) ||
+//       data.category.includes(keyword)
+//   );
+//   res.render("index", { restaurants: restaurants, keyword: keyword });
+// });
 
 // start and listen on the Express server
 app.listen(port, () => {
---
+++ models/Restaurant.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; // 使用mongoose的Schema模組
+
+const restaurantSchema = new Schema({
+  // 建構一個新的Schema
+  name: {
+    type: String, // 資料型別是字串
+    required: true, // 這是個必填欄位
+  },
+  name_en: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  location: { type: String, required: true },
+  phone: { type: String, required: true },
+  google_map: { type: String, required: true },
+  rating: { type: Number, required: true },
+  description: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Restaurant", restaurantSchema);
